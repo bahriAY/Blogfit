6b2f515632fc31cfa0c0e28cb5d6a61a1cf73ef4
Bahri AY <bahri.AY@outlook.fr>
2017-09-12 20:21:21 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import {MdDialog, MdDialogRef, MD_DIALOG_DATA} from '@angular/material';
+import { AuthentificationComponent } from './authentification/authentification.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+constructor(public dialog: MdDialog) {}
+
+// Dialog pour la connexion
+  openDialog(): void {
+  let dialogRef = this.dialog.open(AuthentificationComponent, {
+  height: '220px',
+  width: '400px',
+});
+  }
+
+ // Grid 
+    tiles = [
+    {text: 'Admin', cols: 4, rows: 2, color: 'gray'},
+    {text: 'Mur', cols: 3, rows: 20, color: '#DDBDF1'},
+    {text: 'Messagerie instantannée', cols: 1, rows: 20, color: 'lightgreen'}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdSidenavModule} from '@angular/material';
+import {MdToolbarModule} from '@angular/material';
+import {MdIconModule} from '@angular/material';
+import {MdButtonModule} from '@angular/material';
+import {MdDialogModule} from '@angular/material';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { FormsModule, ReactiveFormsModule }         from '@angular/forms';
+import {MdInputModule} from '@angular/material';
+import {MdGridListModule} from '@angular/material';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthentificationComponent,
+
   ],
+  entryComponents: [AuthentificationComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdButtonModule,
+    MdDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MdInputModule,
+    MdGridListModule
   ],
   providers: [],
+  exports: [FormsModule, ReactiveFormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
