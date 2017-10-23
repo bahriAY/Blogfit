bad29eeb63a77483bd96be7f6f571520099e61bf
Bahri AY <bahri.AY@outlook.fr>
2017-10-23 22:55:47 +0200

    Redesigned

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import {MdDialog, MdDialogRef, MD_DIALOG_DATA} from '@angular/material';
+import { MdDialog, MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { DsService } from './services/ds.service';
 
@@ -8,33 +8,34 @@ import { DsService } from './services/ds.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   username;
   text;
   chats;
   chatArray = [];
   connectedUsers = [];
-  constructor(public dialog: MdDialog, private ds: DsService) {}
   newuser;
 
-// Dialog pour la connexion ( pop up connexion )
+  constructor(public dialog: MdDialog, private ds: DsService) { }
+
+
+  // Dialog pour la connexion ( pop up connexion )
   openDialog(): void {
-  let dialogRef = this.dialog.open(AuthentificationComponent, {
-  height: '220px',
-  width: '400px',
-});
+    let dialogRef = this.dialog.open(AuthentificationComponent, {
+      height: '220px',
+      width: '400px',
+    });
   }
 
   // Ajout d'un message au chat
   addChat() {
-
     const recordName = 'chat/' + this.ds.dsInstance.getUid();
-
     const chatRecord = this.ds.getRecord(recordName);
-    chatRecord.set({username: this.username, text: this.text});
+    chatRecord.set({ username: this.username, text: this.text });
     this.text = '';
     // Update the chats list
+
     this.chats.addEntry(recordName);
   }
 
@@ -46,36 +47,36 @@ export class AppComponent implements OnInit{
 
     // Recupération de l'username saisie
     const username = window.prompt('Veuillez entrer votre surnom pour le chat instantannée:', defaultUsername);
-    
-    // Remplacement du défaut par l'username saisie
-    this.username = username || defaultUsername    
 
+    // Remplacement du défaut par l'username saisie
+    this.username = username || defaultUsername;
+    
     // Login without credentials
     this.ds.login(null, this.loginHandler);
-
+    
+    // Récupère la liste des messages à partir du moment où on s'est identifié
     this.chats = this.ds.getList('chats');
 
+    // Aprés la connexion au tchat en tant que visiteur
     this.chats.on('entry-added', recordName => {
-            
-      this.ds.getRecord( recordName ).whenReady( record => {
 
-        record.subscribe( (data) => {
-          if(data.username && data.text) {
+      this.ds.getRecord(recordName).whenReady(record => {
+
+        record.subscribe((data) => {
+          if (data.username && data.text) {
             // Update bindable property  
             this.chatArray.push(data);
           }
-        }, true );
-
+        }, true);
       });
     })
-
-    this.updateConnectedUser(this.username);
   }
 
-  updateConnectedUser(newuser){
-  this.connectedUsers.push(newuser);
+  getConnectedUserList() {
+    
   }
-
+ // this.updateConnectedUser(this.username);
+  
   loginHandler(success, data) {
     console.log('logged in', success, data);
   }
